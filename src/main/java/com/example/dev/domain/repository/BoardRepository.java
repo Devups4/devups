2b2eea360e329eb1e4d49f8359a53b87e7f676a3
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2021-09-17 17:43:30 +0900

    Delete src directory

--- src/main/java/com/example/dev/domain/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dev.domain.repository;
-
-import com.example.dev.domain.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-}
