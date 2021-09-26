4207ac7036ab2033e043a0423cf3d0cc49355014
ParkSungHyun <psh1030.dev@gmail.com>
2021-09-26 15:11:16 +0900

    modify Notifymodal css

--- src/Component/NotifyModal/style.js
+++ src/Component/NotifyModal/style.js
@@ -4,6 +4,7 @@ export const NotifyModalWrapper = styled.div`
   border-radius: 10px;
   box-shadow: 0px 0px 11px 4px #d6d6d6;
   padding: 30px;
+  background: white;
   position: absolute;
   top: 80px;
   right: 12%;
